f23856732a8fc096418bc3857739d7540ec46ff1
RootA <amwathi@fluidtechglobal.com>
2019-08-15 22:20:27 +0900

    php script changes

--- mail.php
+++ mail.php
@@ -1,11 +1,10 @@
 <?php
     // Check if the form is submitted 
-    if ( isset( $_POST['submit'] ) ) {
-        echo '<h3>Form POST Method</h3>'; 
+    if ( isset($_POST['submit'] ) ) {
         $now = date("Y/m/d h:i:sa");
+        $email = $_REQUEST['email'];
         $txt = " [. $now .] . $email .\n";
         fwrite($logfile, $txt);
         fclose($logfile);
         header("Location: http://work4fce.com");
-        exit; 
     } 
