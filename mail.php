f05092ee7ea929faeca6bf1bcb9b6ad3c0b87f47
RootA <amwathi@fluidtechglobal.com>
2019-08-15 21:57:06 +0900

    D

--- mail.php
+++ mail.php
@@ -1,12 +1,11 @@
 <?php
     // Check if the form is submitted 
-    if ( isset( $_POST['submit'] ) ) {
+    if ( isset( $_GET['submit'] ) ) {
         echo '<h3>Form POST Method</h3>'; 
-        echo 'Your name is ' . $lastname . ' ' . $firstname; 
-        $logfile = fopen("newfile.txt", "w") or die("Unable to open file!");
         $now = date("Y/m/d h:i:sa");
         $txt = " [. $now .] . $email .\n";
         fwrite($logfile, $txt);
         fclose($logfile);
+        header("Location: http://work4fce.com");
         exit; 
     } 
