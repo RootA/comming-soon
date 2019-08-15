ca7f688cf0986523f93567a75565986899f7d7f4
RootA <amwathi@fluidtechglobal.com>
2019-08-15 22:24:19 +0900

    php script changes

--- mail.php
+++ mail.php
@@ -4,6 +4,7 @@
         $now = date("Y/m/d h:i:sa");
         $email = $_REQUEST['email'];
         $txt = " [. $now .] . $email .\n";
+        $logfile = fopen("emails.log", "w") or die("Unable to open file!");
         fwrite($logfile, $txt);
         fclose($logfile);
         header("Location: http://work4fce.com");
